bc8689b9fa04ad3f7af1e47bb577331615bde797
cleavonsurumo <cleavonsurumo@gmail.com>
2025-11-28 13:06:30 +0300

    Fix Link import in Contact2

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Layout from "@/components/layout/Layout"
-import Blog1 from "@/components/sections/Blog1"
+import Blog2 from "@/components/sections/Blog2"
 import Brands1 from "@/components/sections/Brands1"
 import Contact2 from "@/components/sections/Contact2"
 import Home2 from "@/components/sections/Home2"
 import Projects1 from "@/components/sections/Projects1"
 import Service2 from "@/components/sections/Service1"
 import Skills2 from "@/components/sections/Skills2"
-import Static1 from "@/components/sections/Static1"
+import Static2 from "@/components/sections/Static2"
 import Resume1 from "@/components/sections/Resume1"
 import Testimonials1 from "@/components/sections/Testimonials1"
 
@@ -16,14 +16,14 @@ export default function Home() {
 		<>
 			<Layout headerStyle={1} footerStyle={1}>
 				<Home2 />
-				<Static1 />
+				<Static2 />
 				<Service2 />
 				<Projects1 />
 				<Resume1 />
 				<Skills2 />
 				<Brands1 />
 				<Testimonials1 />
-				<Blog1 />
+				<Blog2 />
 				<Contact2 />
 			</Layout>
 		</>
