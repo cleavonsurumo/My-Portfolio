c93aeb866b95e1d8e2ab177d04a2aa093d46bfa8
cleavonsurumo <cleavonsurumo@gmail.com>
2025-12-25 13:21:51 +0300

    updating mobile menu

--- components/layout/MobileMenu.tsx
+++ components/layout/MobileMenu.tsx
@@ -28,19 +28,19 @@ export default function MobileMenu({ isMobileMenu, handleMobileMenu }: any) {
 											<Link className="nav-link active" href="/">Home</Link>
 										</li>
 										<li className="nav-item">
-											<Link className="nav-link" href="/services">Services</Link>
+											<Link className="nav-link" href="#services">Services</Link>
 										</li>
 										<li className="nav-item">
-											<Link className="nav-link" href="/work">Portfolio</Link>
+											<Link className="nav-link" href="#portfolio">Portfolio</Link>
 										</li>
 										<li className="nav-item">
 											<Link className="nav-link" href="/pricing">Pricing</Link>
 										</li>
 										<li className="nav-item">
-											<Link className="nav-link" href="/blog-list">Blog</Link>
+											<Link className="nav-link" href="#blog">Blog</Link>
 										</li>
 										<li className="nav-item">
-											<Link className="nav-link" href="/#contact">Contact</Link>
+											<Link className="nav-link" href="#contact">Contact</Link>
 										</li>
 									</ul>
 								</nav>
